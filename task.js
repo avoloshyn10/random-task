2b17c63eb451127f5f236a681a0916e3c4cf188f
avoloshyn10 <76859180+avoloshyn10@users.noreply.github.com>
2021-11-26 20:46:27 +0200

    Update task.js

--- task.js
+++ task.js
@@ -2,4 +2,9 @@ function task()
 {
   var first = Math.ceil(Math.random()*5);
   console.log("Обязательные задачи:" + first);
+  switch(first)
+  {
+    case 1:
+      console.log("1. Обычная задача");
+  }
 }

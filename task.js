c0cc43188242f89523e63c3933a26a507e3480af
avoloshyn10 <76859180+avoloshyn10@users.noreply.github.com>
2021-11-28 12:37:20 +0200

    Update task.js

--- task.js
+++ task.js
@@ -27,5 +27,38 @@ function task()
     case 1:
       console.log("1. Программирование");
       break;
+    case 2:
+      console.log("2. ");
+      break;
+    case 2:
+      console.log("2. ");
+      break;
+    case 2:
+      console.log("2. ");
+      break;
+    case 2:
+      console.log("2. ");
+      break;
+    case 2:
+      console.log("2. ");
+      break;
+    case 2:
+      console.log("2. ");
+      break;
+    case 2:
+      console.log("2. ");
+      break;
+    case 2:
+      console.log("2. ");
+      break;
+    case 2:
+      console.log("2. ");
+      break;
+    case 2:
+      console.log("2. ");
+      break;
+    case 2:
+      console.log("2. ");
+      break;
   }
 }

f657caca69e09c3125598243f722d6932ddad181
avoloshyn10 <76859180+avoloshyn10@users.noreply.github.com>
2021-11-29 09:31:18 +0200

    Update task.js

--- task.js
+++ task.js
@@ -3,6 +3,8 @@ function task()
   var first = Math.ceil(Math.random()*5);
   console.log("Обязательные задачи:" + first);
   var first_arr = ["1. Обычная задача", "2. English", "3. OpenPanzer2", "4. Постирать носки, трусы, футболку, побриться", "5. Random task, RR3 50000 points"];
+  console.log(first_arr[first-1]);
+  /*
   switch(first)
   {
     case 1:
@@ -21,6 +23,7 @@ function task()
       console.log("5. Random task, RR3 50000 points");
       break;
   }
+  */
   var second = Math.ceil(Math.random()*12);
   console.log("Обычные задачи:" + second);
   switch(second)
